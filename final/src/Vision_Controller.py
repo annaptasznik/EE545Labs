dcaa864f2e5a36f83c4ce6aac142fb7561b70801
scotthem <scotthem@uw.edu>
2018-12-09 21:24:43 -0800

    Vision controller image criteria now in launch file

--- final/src/Vision_Controller.py
+++ final/src/Vision_Controller.py
@@ -19,7 +19,7 @@ IMAGE_TOPIC = '/camera/color/image_processed'
 class Vision_Controller:
     
     # Initializer
-    def __init__(self, lowerLimit, upperLimit, speed, pixel_to_angle):
+    def __init__(self, lowerLimit, upperLimit, min_area, speed, pixel_to_angle):
         self.sub = rospy.Subscriber(CAMERA_TOPIC, Image, self.image_process_cb, queue_size = 1)
         self.controller_pub = rospy.Publisher(CONTROL_TOPIC, AckermannDriveStamped, queue_size = 1)
         self.image_pub = rospy.Publisher(IMAGE_TOPIC, Image, queue_size = 1)
@@ -28,8 +28,11 @@ class Vision_Controller:
         self.upperLimit = upperLimit
         self.lowerLimit = lowerLimit
         
+        self.min_area = min_area
         self.speed = speed
         self.pixel_to_angle = pixel_to_angle
+
+        self.counter = 0
         
     # Callback to process the incoming image from the camera    
     def image_process_cb(self, msg):
@@ -37,15 +40,13 @@ class Vision_Controller:
         x, y = self.color_track(msg)
  
         # Interpret the x,y position and make appropriate control
-        if((x != 0 and y !=0) and (y < 360) and (x < 320 or x < 400)):
-            print 'blue object detected'       
+        if((x != 0 and y !=0) and (y < 360) and (x < 280 or x < 360)):
+            print 'blue object detected', self.counter, x, y
+            self.counter += 1
             ads = self.compute_steering(x, y)
             self.controller_pub.publish(ads)
 
     def color_track(self, msg): 
-        # minimum size to be considered a valid object
-        size_limit = 100000 
-
         # Convert to CV2 image
         try:
             image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
@@ -59,12 +60,12 @@ class Vision_Controller:
         moments = cv2.moments(mask_image, 0)
         area    = moments['m00']
 
-        if(area > size_limit):
+        if(area > self.min_area):
             x = int(moments['m10'] / area)
             y = int(moments['m01'] / area)
         
         else:
-            x = 0
+            x = 0 
             y = 0
 
         # Publish the image so we can view it for debug
@@ -86,11 +87,15 @@ class Vision_Controller:
         
 def main():
     rospy.init_node('Vision_Controller', anonymous=True)
+    R_value = rospy.get_param("~R_value", 70)
+    G_value = rospy.get_param("~G_value", 120)
+    B_value = rospy.get_param("~B_value", 175)
+    min_area = rospy.get_param("~min_area", 50000)
     speed = rospy.get_param("~speed", 1.0)
     pixel_to_angle = rospy.get_param("~pixel_to_angle", 0.0014)
     
     # Upper and lower limit for blue values
-    blue = np.uint8([[[70, 120, 175]]])
+    blue = np.uint8([[[R_value, G_value, B_value]]])
     hsvBlue = cv2.cvtColor(blue,cv2.COLOR_BGR2HSV)
     print(hsvBlue)
     
@@ -99,7 +104,7 @@ def main():
     print(upperLimit)
     print(lowerLimit)
                                      
-    vc = Vision_Controller(lowerLimit, upperLimit, speed, pixel_to_angle)
+    vc = Vision_Controller(lowerLimit, upperLimit, min_area, speed, pixel_to_angle)
     rospy.spin()
 
 if __name__ == '__main__':
